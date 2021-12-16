7ae22e401bc5e6ca0eb5e4fd9d0823d83512d22c
Ivan Andrianov <i.a.andrianov@gmail.com>
2021-12-16 20:35:06 +0300

    admin-only basic auth, no csrf and form auth

---
+++ src/main/kotlin/sestra/common/rest/WebSecurityConfig.kt
@@ -0,0 +1,45 @@
+package sestra.common.rest
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig(
+    @Value("password.admin") private val adminPassword: String
+) : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+            .antMatchers("/api/**")
+            .authenticated()
+            .anyRequest()
+            .permitAll()
+
+        http.httpBasic()
+        http.csrf().disable()
+        http.formLogin().disable()
+    }
+
+    @Bean
+    override fun userDetailsService(): UserDetailsService {
+        val user = User.builder()
+            .username("admin")
+            .password(adminPassword)
+            .roles("ADMIN")
+            .passwordEncoder(passwordEncoder()::encode)
+            .build()
+
+        return InMemoryUserDetailsManager(user)
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+}
